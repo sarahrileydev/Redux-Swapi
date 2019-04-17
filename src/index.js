6c00bd7df5406d6ed5df924f730d4b33352046db
sriley <sarahriley.dev@gmail.com>
2019-04-17 15:57:27 -0400

    added imports and middleware

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
-// needed dependancies
+// needed dependencies
 // applyMiddleware from redux
+import applyMiddleware from "redux";
 // thunk from redux-thunk
+import thunk from "redux-thunk";
 // logger from redux-logger
+import logger from "redux-logger";
 // rootReducer from ./reducers
+import rootReducer from "./reducers";
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
